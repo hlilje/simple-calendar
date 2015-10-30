9064f191a3f9df183bd9a3b76e8cfa71456f437e
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-10-30 14:35:03 +0100

    Add WesternDate class

--- date.hpp
+++ date.hpp
@@ -8,31 +8,22 @@ namespace lab2 {
         private:
         public:
             Date();
-            Date(const Date & d);
+            Date(const Date& d);
             virtual ~Date();
 
-            virtual Date& operator++();
-            virtual Date operator++(int);
-            virtual Date& operator--();
-            virtual Date operator--(int);
-            virtual Date& operator-(const Date& rhs);
-            virtual Date& operator=(const Date& rhs);
-            virtual Date& operator+=(const Date& rhs);
-            virtual Date& operator-=(const Date& rhs);
-            virtual bool operator<(const Date& rhs); // TODO: More?
+            virtual Date& operator++() = 0;
+            virtual Date& operator--() = 0;
+            virtual Date& operator-(const Date& rhs) = 0;
+            virtual Date& operator=(const Date& rhs) = 0;
+            virtual Date& operator+=(const Date& rhs) = 0;
+            virtual Date& operator-=(const Date& rhs) = 0;
+            virtual bool operator==(const Date& rhs) = 0;
+            virtual bool operator!=(const Date& rhs) = 0;
+            virtual bool operator<(const Date& rhs) = 0;
+            virtual bool operator<=(const Date& rhs) = 0;
+            virtual bool operator>(const Date& rhs) = 0;
+            virtual bool operator>=(const Date& rhs) = 0;
 
-            virtual int year();
-            virtual unsigned int month();
-            virtual unsigned int day();
-            virtual unsigned int week_day();
-            virtual unsigned int days_per_week();
-            virtual unsigned int days_this_month();
-            virtual std::string week_day_name();
-            virtual std::string month_name();
-            virtual void add_year(int n = 1); // TODO: Unspecified return type
-            virtual void add_month(int n = 1); // TODO: Unspecified return type
-            virtual int mod_julian_day();
-
-            friend std::ostream& operator<<(std::ostream& os, const Date& d);
+            /* friend std::ostream& operator<<(std::ostream& os, const Date& d); */
     };
 }
---
+++ western_date.cpp
@@ -0,0 +1,1 @@
+#include "western_date.hpp"
