2ffef7c214f010a38274d98961720e0b7c03ac32
clid <clid@kth.se>
2015-11-02 10:34:11 +0100

    added comments

--- gregorian.hpp
+++ gregorian.hpp
@@ -9,6 +9,7 @@ namespace lab2 {
         private:
             // Return true if the current year is a leap year.
             bool is_leap_year();
+            // Convert Julian Day Number to Julian Date
             void jdn_to_gregorian_date(const long jdn, unsigned int & year,
                                        unsigned int & month, unsigned int & day);
 
