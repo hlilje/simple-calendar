35f7d51cdc934f5973d463afb5774b8bdc31ebfd
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-11-02 11:43:03 +0100

    Add copy test

--- test_calendar.cpp
+++ test_calendar.cpp
@@ -7,7 +7,7 @@ using namespace lab2;
 
 class CalendarSuite : public CxxTest::TestSuite {
     public:
-        void test_initialisation() {
+        void test_default_init() {
             time_t tp;
             time(&tp);
             set_k_time(tp);
@@ -26,15 +26,51 @@ class CalendarSuite : public CxxTest::TestSuite {
             tp = 0;
             set_k_time(tp);
 
-            const int j_year  = 1969;
-            const int j_month = 12;
-            const int j_day   = 19;
-
             Julian j;
 
-            TS_ASSERT_EQUALS((int) j.year(), j_year);
-            TS_ASSERT_EQUALS((int) j.month(), j_month);
-            TS_ASSERT_EQUALS((int) j.day(), j_day);
+            TS_ASSERT_EQUALS((int) j.year(), 1969);
+            TS_ASSERT_EQUALS((int) j.month(), 12);
+            TS_ASSERT_EQUALS((int) j.day(), 19);
+        }
+
+        void test_copy_init() {
+            time_t tp;
+            time(&tp);
+            set_k_time(tp);
+
+            Gregorian g1;
+            Gregorian g2(g1);
+            Gregorian g3 = g1;
+            Gregorian g4; g4 = g1;
+
+            TS_ASSERT_EQUALS(g1.year(), g2.year());
+            TS_ASSERT_EQUALS(g1.month(), g2.month());
+            TS_ASSERT_EQUALS(g1.day(), g2.day());
+
+            TS_ASSERT_EQUALS(g1.year(), g3.year());
+            TS_ASSERT_EQUALS(g1.month(), g3.month());
+            TS_ASSERT_EQUALS(g1.day(), g3.day());
+
+            TS_ASSERT_EQUALS(g1.year(), g4.year());
+            TS_ASSERT_EQUALS(g1.month(), g4.month());
+            TS_ASSERT_EQUALS(g1.day(), g4.day());
+
+            Julian j1;
+            Julian j2(j1);
+            Julian j3 = j1;
+            Julian j4; j4 = j1;
+
+            TS_ASSERT_EQUALS(j1.year(), j2.year());
+            TS_ASSERT_EQUALS(j1.month(), j2.month());
+            TS_ASSERT_EQUALS(j1.day(), j2.day());
+
+            TS_ASSERT_EQUALS(j1.year(), j3.year());
+            TS_ASSERT_EQUALS(j1.month(), j3.month());
+            TS_ASSERT_EQUALS(j1.day(), j3.day());
+
+            TS_ASSERT_EQUALS(j1.year(), j4.year());
+            TS_ASSERT_EQUALS(j1.month(), j4.month());
+            TS_ASSERT_EQUALS(j1.day(), j4.day());
         }
 
         void test_date_constructors() {
