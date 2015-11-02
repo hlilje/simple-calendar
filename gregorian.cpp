322bbb4349387fdd212b22988a6a3fe6d89ac3a5
clid <clid@kth.se>
2015-11-02 10:26:45 +0100

    added jdn to date functions

--- gregorian.cpp
+++ gregorian.cpp
@@ -58,3 +58,29 @@ int lab2::Gregorian::mod_julian_day() {
 bool lab2::Gregorian::is_leap_year() {
     return (_year % 4 == 0) && ((_year % 100) != 0 || (_year % 400) == 0);
 }
+
+void lab2::Gregorian::jdn_to_gregorian_date(const long jdn,
+                                            unsigned int & year,
+                                            unsigned int & month,
+                                            unsigned int & day) {
+    unsigned int y = 4716;
+    unsigned int j = 1401;
+    unsigned int m = 2;
+    unsigned int n = 12;
+    unsigned int r = 4;
+    unsigned int p = 1461;
+    unsigned int v = 3;
+    unsigned int u = 5;
+    unsigned int s = 153;
+    unsigned int w = 2;
+    unsigned int B = 274277;
+    unsigned int C = 38;
+
+    unsigned int f = jdn + j + (((((4 * jdn) + B) / 146097) * 3) / 4) - C;
+    unsigned int e = (r * f) + v;
+    unsigned int g = (e % p) / r;
+    unsigned int h = (u * g) + w;
+    day = ((h % s) / u) + 1;
+    month = (((h / s) + m) % n) + 1;
+    year = (e / p) - y + ((n + m - month) / n);
+}
--- julian.cpp
+++ julian.cpp
@@ -53,3 +53,29 @@ int lab2::Julian::mod_julian_day() {
 bool lab2::Julian::is_leap_year() {
     return _year % 4 == 0;
 }
+
+void lab2::Gregorian::jdn_to_julian_date(const long jdn,
+                                         unsigned int & year,
+                                         unsigned int & month,
+                                         unsigned int & day) {
+    unsigned int y = 4716;
+    unsigned int j = 1401;
+    unsigned int m = 2;
+    unsigned int n = 12;
+    unsigned int r = 4;
+    unsigned int p = 1461;
+    unsigned int v = 3;
+    unsigned int u = 5;
+    unsigned int s = 153;
+    unsigned int w = 2;
+    unsigned int B = 274277;
+    unsigned int C = 38;
+
+    unsigned int f = jdn + j;
+    unsigned int e = (r * f) + v;
+    unsigned int g = (e % p) / r;
+    unsigned int h = (u * g) + w;
+    day = ((h % s) / u) + 1;
+    month = (((h / s) + m) % n) + 1;
+    year = (e / p) - y + ((n + m - month) / n);
+}
