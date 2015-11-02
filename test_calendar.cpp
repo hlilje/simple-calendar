23889e45a2e87518529043f03497ef1c0edf7af3
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-11-02 10:28:12 +0100

    Test default constructor of Gregorian

--- test_calendar.cpp
+++ test_calendar.cpp
@@ -13,7 +13,16 @@ class CalendarSuite : public CxxTest::TestSuite {
             time(&tp);
             set_k_time(tp);
 
+            const struct tm *t = gmtime(&tp);
+            const long year  = t->tm_year + 1900;
+            const long month = t->tm_mon + 1;
+            const long day   = t->tm_mday;
+
             Julian j;
             Gregorian g;
+
+            TS_ASSERT_EQUALS(g.year(), year);
+            TS_ASSERT_EQUALS(g.month(), month);
+            TS_ASSERT_EQUALS(g.day(), day);
         }
 };
